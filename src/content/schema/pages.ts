c3091a791ec44f0b52a511f2661936aa53f6e488
Ronald Diemicke <ron.diemicke@gm.com>
2024-11-10 11:40:14 -0600

    simplifying this for testing

---
+++ src/content/schema/pages.ts
@@ -0,0 +1,12 @@
+// 2. Define a `type` and `schema` for each collection
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+export const pageCollection = defineCollection({
+    type: 'content', // v2.5.0 and later
+    schema: z.object({
+        title: z.string(),
+        tags: z.array(z.string()),
+        image: z.string().optional(),
+    }),
+});
