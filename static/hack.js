dad0b335c067f7a84a81b07d0c89bb17cb7b56ad
CloudCannon <support@cloudcannon.com>
2024-11-10 18:28:53 +0000

    Updated 3 files via CloudCannon

--- static/hack.js
+++ static/hack.js
@@ -12,4 +12,18 @@
     }
 
     customElements.define("gb-hfpg-colorizer", GB_HF_PG_Colorizer);
+
+    if (!window.CloudCannon) {
+        document.addEventListener('cloudcannon:load', function (e) {
+            onLiveEditorLoad(e.detail.CloudCannon);
+        });
+    } else {
+        onLiveEditorLoad(window.CloudCannon);
+    }
+    function onLiveEditorLoad(CloudCannon) {
+        CloudCannon.enableEvents();
+    }
+    document.addEventListener('cloudcannon:update', async function (e) {
+        useNewPageProps(e.detail.CloudCannon);
+    });
 })();
