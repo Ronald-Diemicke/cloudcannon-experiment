c4c97bf5378a44869a05eaf55d75b3bf785a723a
Ronald Diemicke <ron.diemicke@gm.com>
2024-11-11 21:56:57 -0600

    making this able to traverse the object

--- public/static/hack.js
+++ public/static/hack.js
@@ -14,18 +14,32 @@
     customElements.define("gb-hfpg-colorizer", GB_HF_PG_Colorizer);
 
     //cloudcannon live preview
+    function getReturnValue(strKey, obj) {
+        if(strKey.includes('.')){
+            let strKeyArray = strKey.split('.');
+            let nextObj = obj[strKeyArray[0]];
+            if(typeof nextObj === 'object' && nextObj !== null) {
+                traverseObject(strKeyArray.shift().join('.'), nextObj);
+            } else {
+                return nextObj;
+            }
+        } else {
+            return obj[strKey];
+        }
+    }
+
     async function useNewPageProps(CloudCannon) {
         const latestValue = await CloudCannon.value();
         document.querySelectorAll('[data-cms-bind]').forEach((el) => {
             const key = el.dataset['cmsBind'].slice(1);
-            let newValue = latestValue[key];
+            let newValue = getReturnValue(key,latestValue);
             if(el.dataset['cmsBindAttribute']) {
                 if(el.dataset['cmsBindAttribute'] === 'src') {
                     newValue = `/static${newValue}`;
                 }
                 el.setAttribute(el.dataset['cmsBindAttribute'], newValue);
             } else {
-                el.innerHTML = latestValue[key];
+                el.innerHTML = getReturnValue(key,latestValue);
             }
         })
     }
