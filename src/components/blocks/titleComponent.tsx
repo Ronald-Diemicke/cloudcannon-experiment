c3a88e15cbb2ac560db4637f73029c205829d89c
Ronald Diemicke <ron.diemicke@gm.com>
2024-11-11 23:18:30 -0600

    fix

--- src/components/blocks/titleComponent.tsx
+++ src/components/blocks/titleComponent.tsx
@@ -8,8 +8,8 @@ interface Props {
 export default function titleComponent({ block }) {
     return (
         <>
-            <h1 className="gb-headline1" data-cms-bind="#titleComponent.title">{block.title}</h1>
-            <div data-cms-bind="#titleComponent.body">{block.body}</div>
+            <h1 className="gb-headline1" data-cms-bind="#blocksSection.titleComponent.title">{block.title}</h1>
+            <div data-cms-bind="#blocksSection.titleComponent.body">{block.body}</div>
         </>
     );
 };
