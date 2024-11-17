20f65371835efd94f6de8f1895e7dda4e8d6fd88
Ronald Diemicke <ron.diemicke@gm.com>
2024-11-17 12:28:16 -0600

    Fixed schema

--- src/content/schema/pages.ts
+++ src/content/schema/pages.ts
@@ -5,10 +5,7 @@ import { z, defineCollection } from 'astro:content';
 export const pageCollection = defineCollection({
     type: 'content', // v2.5.0 and later
     schema: z.object({
-        content: z.string(),
-        tags: z.array(z.string()),
-        image: z.string().optional(),
-        blocksSection: z.array(z.any()),
+        title: z.string(),
         content_blocks: z.array(z.any())
     })
 });
